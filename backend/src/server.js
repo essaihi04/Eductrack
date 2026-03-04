45ef2f62b23ff60e3ba4f120fe5e9ba88831535f
essaihi04 <essaihi04@gmail.com>
2026-03-04 00:33:37 +0000

    Add more localhost ports to CORS whitelist (5174, 5175, etc.)

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.use(helmet({
   crossOriginResourcePolicy: { policy: 'cross-origin' },
 }));
 app.use(cors({
-  origin: ['https://etrack.ma', 'http://localhost:5173'],
+  origin: ['https://etrack.ma', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
